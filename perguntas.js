84ade1b9f0e81a5c3be01f5dc3f118126d07da2d
verde27 <168455475+verde27@users.noreply.github.com>
2024-11-14 07:30:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'biologia',
+    'O que é sustentabilidade?',
+    'é o desenvolvimento capaz de suprir as necessidades da geração atual, garantindo a capacidade de atender as necessidades das futuras gerações'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de londres?',
+    'A capital de londres é reino unido'
+)
+
+criaCartao(
+    'ciencias',
+    'O que é um furacão?',
+    'são um tipo de ciclone tropical formado por um centro de baixa pressão atmosférica.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'oque siguinifica god is the way?',
+    ' siguinifica deus é o caminho'
+)
